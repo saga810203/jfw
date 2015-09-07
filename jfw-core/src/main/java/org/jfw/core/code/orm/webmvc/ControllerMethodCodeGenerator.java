f69d7b144ffd2d00ac941d3fda4509e4cb695720
pengjia <pengjia@isoftstone.com>
2015-09-07 18:12:06 +0800

    --

---
+++ jfw-core/src/main/java/org/jfw/core/code/orm/webmvc/ControllerMethodCodeGenerator.java
@@ -0,0 +1,22 @@
+package org.jfw.core.code.orm.webmvc;
+
+import java.lang.reflect.Method;
+
+import org.jfw.core.code.MethodCodeGenerator;
+
+public class ControllerMethodCodeGenerator implements MethodCodeGenerator{
+	
+
+	@Override
+	public void init(Class<?> parentType, Method method) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String build() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
