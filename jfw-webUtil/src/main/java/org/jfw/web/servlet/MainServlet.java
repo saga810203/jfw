98de2e1f99f0641bb44d5925936fc7f1a122f393
pengjia <pengjia@isoftstone.com>
2015-09-19 12:51:31 +0800

    --

--- jfw-webUtil/src/main/java/org/jfw/web/servlet/MainServlet.java
+++ jfw-webUtil/src/main/java/org/jfw/web/servlet/MainServlet.java
@@ -16,11 +16,21 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.jfw.util.WebUtil;
+
 
 public class MainServlet extends HttpServlet {
     private static final long serialVersionUID = -2950792293111339623L;
+    private static final Class<?>[] CTRL_METHOD_PARAM_TYPE=new Class<?>[]{HttpServletRequest.class,HttpServletResponse.class};
+    public static final String REQ_MATCH_URI ="org.jfw.web.reqMacthUri";
+    public static final String REQ_MATCH_URI_DYN ="org.jfw.web.reqMacthUri_DYN";
+    
+    
     private static final Map<String,ControllerMethod> staticUrls = new HashMap<String,ControllerMethod>();
-    private static final ControllerMethod[][] dynamicUrls = new ControllerMethod[100][];
+    private static final ControllerMethod[][] dynamicUrls = new ControllerMethod[100][0];
+    
+    
+    public static int prefixLen = 0;
     
     private Properties readDispather()
     {
@@ -62,10 +72,80 @@ public class MainServlet extends HttpServlet {
         return result;
     }
 
+    private void addControllerMethod(String methodName,String url,Object ctrl) throws Exception{
+        Method method =null;        
+        try {
+            method =ctrl.getClass().getMethod(methodName, CTRL_METHOD_PARAM_TYPE);
+        } catch (Exception e) {
+            this.getServletContext().log("load controll error:"+ctrl.getClass().getName()+"."+methodName, e);
+            throw e;
+        }  
+        String[] urlp = url.split("/");
+        for(String str:urlp)
+        {
+            if(str.startsWith("{")&& str.endsWith("}")){
+                this.addDynamicUrlControllerMehtod(urlp, method, ctrl);
+                return;
+            }
+        }
+        staticUrls.put(url,new ControllerMethod(ctrl,method));        
+    }
+    private void addDynamicUrlControllerMehtod(String[] url,Method method,Object ctrl){
+        for( int i = 0 ; i < url.length ; ++i){
+            if(url[i].startsWith("{")&& url[i].endsWith("}")){
+                url[i] = null;
+            }
+        }
+        ControllerMethod[] cms = dynamicUrls[url.length];
+        if(cms.length == 0){
+            cms = new ControllerMethod[1];
+            cms[0] = new ControllerMethod(ctrl,method,url);
+            dynamicUrls[url.length] = cms;
+        }else{
+            dynamicUrls[url.length] = new ControllerMethod[cms.length+1];
+            System.arraycopy(cms,0,dynamicUrls[url.length],0,cms.length);
+            dynamicUrls[url.length][cms.length]= new ControllerMethod(ctrl,method,url);
+        }        
+    }
+
+    
+    private void buildControllerMethods(Properties props) throws Exception
+    {
+        Map<String,Object> map = this.createController(props);
+        for(Map.Entry<Object,Object> entry:props.entrySet()){
+            String url =((String)entry.getKey()).trim();
+            String value =((String) entry.getValue()).trim();
+            int index = value.indexOf(":");
+            String className = value.substring(0,index).trim();
+            String method = value.substring(index+1).trim();
+            Object ctrl = map.get(className);
+            if(null!=ctrl){
+                this.addControllerMethod(method, url, ctrl);
+            }
+        }
+    }
+   
+
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        // TODO Auto-generated method stub
-        super.service(req, resp);
+        String reqUri = WebUtil.normalize(req.getRequestURI());
+        reqUri = reqUri.substring(prefixLen);
+        req.setAttribute(REQ_MATCH_URI, reqUri);
+        ControllerMethod cm = staticUrls.get(reqUri);
+        if(null!=cm){
+            cm.invoke(req, resp);
+        }else{
+            String[] reqUris = reqUri.split("/");
+            ControllerMethod[] cms = dynamicUrls[reqUris.length];
+            for(int i = 0 ; i < cms.length ; ++i){
+                if(cms[i].match(reqUris)){
+                    req.setAttribute(REQ_MATCH_URI_DYN, reqUris);
+                    cms[i].invoke(req, resp);
+                    return;
+                }
+            }
+           resp.sendError(404); 
+        }
     }
 
     @Override
@@ -75,8 +155,14 @@ public class MainServlet extends HttpServlet {
 
     @Override
     public void init() throws ServletException {
-        // TODO Auto-generated method stub
-        super.init();
+
+
+        try {
+            this.buildControllerMethods(this.readDispather());
+        } catch (Exception e) {
+            // TODO Handler Exception
+            e.printStackTrace();
+        }
     }
     private static class ControllerMethod{
         private Object obj;
@@ -103,18 +189,7 @@ public class MainServlet extends HttpServlet {
             return true;
         }
         
-        public Object getObj() {
-            return obj;
-        }
-        public void setObj(Object obj) {
-            this.obj = obj;
-        }
-        public Method getMethod() {
-            return method;
-        }
-        public void setMethod(Method method) {
-            this.method = method;
-        }
+      
         public void invoke(HttpServletRequest req,HttpServletResponse res) throws ServletException, IOException{
             try {
                 this.method.invoke(obj, req,res);
